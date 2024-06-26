15d6e80c6ef44b72e23ee8dc93d225250b7232af
Harsha Nawana <harshanawana@gmail.com>
2024-06-26 12:23:27 +0530

    Updated code
    Added some components and created some pages of frontend

---
+++ components/home/LatestBook.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LatestBook = () => {
+  return (
+    <View>
+      <Text>LatestBook</Text>
+    </View>
+  );
+};
+
+export default LatestBook;
---
+++ components/sampleData.jsx
@@ -0,0 +1,96 @@
+import book1 from "../assets/pdf/TheSaffronRobe.pdf";
+import book2 from "../assets/pdf/ChaptersofLife.pdf";
+import book3 from "../assets/pdf/BeyondtheTenth.pdf";
+import book4 from "../assets/pdf/FeedingtheFlame.pdf";
+import book5 from "../assets/pdf/TheHermit.pdf";
+import book6 from "../assets/pdf/TheThirteenthCandle.pdf";
+import book7 from "../assets/pdf/Candlelight.pdf";
+import book8 from "../assets/pdf/Twilight.pdf";
+import book9 from "../assets/pdf/AsitWas.pdf";
+
+import cover1 from "../assets/pdf/TheSaffronRobe.png";
+import cover2 from "../assets/pdf/ChaptersofLife.png";
+import cover3 from "../assets/pdf/BeyondtheTenth.png";
+import cover4 from "../assets/pdf/FeedingtheFlame.png";
+import cover5 from "../assets/pdf/TheHermit.png";
+import cover6 from "../assets/pdf/TheThirteenthCandle.png";
+import cover7 from "../assets/pdf/Candlelight.png";
+import cover8 from "../assets/pdf/Twilight.png";
+import cover9 from "../assets/pdf/AsitWas.png";
+
+export const categories = ["Horrors", "Sci Fi", "Romance", "Business", "ICT"];
+
+export const libraryBooks = [
+  {
+    id: 1,
+    title: "The Saffron Robe",
+    author: "Olivia Wilson",
+    image: cover1,
+    rating: 5.7,
+    pdf: book1,
+  },
+  {
+    id: 2,
+    title: "Chapters of Life",
+    author: "Aruna Kumara",
+    image: cover2,
+    rating: 7.7,
+    pdf: book2,
+  },
+  {
+    id: 3,
+    title: "Beyond the Tenth",
+    author: "Rajiv Malhotra",
+    image: cover3,
+    rating: 8.2,
+    pdf: book3,
+  },
+  {
+    id: 4,
+    title: "Feeding the Flame",
+    author: "Lisa Maxwell",
+    image: cover4,
+    rating: 6.5,
+    pdf: book4,
+  },
+  {
+    id: 5,
+    title: "The Hermit",
+    author: "Emma Davies",
+    image: cover5,
+    rating: 6.9,
+    pdf: book5,
+  },
+  {
+    id: 6,
+    title: "The Thirteenth Candle",
+    author: "Robert Smith",
+    image: cover6,
+    rating: 7.1,
+    pdf: book6,
+  },
+  {
+    id: 7,
+    title: "Candlelight",
+    author: "Jessica Adams",
+    image: cover7,
+    rating: 7.3,
+    pdf: book7,
+  },
+  {
+    id: 8,
+    title: "Twilight",
+    author: "Stephenie Meyer",
+    image: cover8,
+    rating: 8.5,
+    pdf: book8,
+  },
+  {
+    id: 9,
+    title: "As It Was",
+    author: "Elle Turner",
+    image: cover9,
+    rating: 6.8,
+    pdf: book9,
+  },
+];
